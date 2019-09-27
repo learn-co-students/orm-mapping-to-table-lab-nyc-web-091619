68db02ea2657f9f9f38a47ff70f8cd20d9acc2a8
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-27 14:07:06 -0400

    forked and copied files, forgot to do so before, .drop_table passes all tests

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,30 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_reader :id
+  attr_accessor :name, :grade
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      ) 
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
